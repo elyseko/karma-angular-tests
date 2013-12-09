70cda0a4be8b25af1b39657a0aaefb8292714712
Elyse <elyse@realeyes.com>
2013-12-09 10:26:47 -0700

    Tests for play/pause login in controller - in this branch they will fail

---
+++ article1/test/unit/VideoPlayer.Spec.js
@@ -0,0 +1,50 @@
+'use strict';
+
+/* jasmine specs for controllers go here */
+
+describe('Video Player', function(){
+  beforeEach(module('myApp.VideoPlayer'));
+
+  describe(' controller', function(){
+  	var scope;
+  	var rootScope;
+  	beforeEach( 
+			function() {
+				inject(['$rootScope', '$controller', function($rootScope, $controller) {
+					scope = $rootScope.$new();
+					rootScope = $rootScope;
+					$controller('videoPlayerCtrl', {$scope: scope, $rootScope: rootScope});
+				}]);
+			}
+		);
+
+	it(' onPlayPause sets playState to playing', function() {
+		scope.onPlayPause();
+		expect(scope.playState).toBe('playing');
+	});
+
+	it(' onPlayPause sets playState to paused', function() {
+		scope.$apply(function(){
+			scope.playState = 'playing';
+		});
+		scope.onPlayPause();
+		expect(scope.playState).toBe('paused');
+	});
+
+	it(' onPlayPause dispatches playing state', function() {
+		spyOn(rootScope, '$broadcast');
+		scope.onPlayPause();
+		expect(scope.$broadcast).toHaveBeenCalledWith('playStateEvent', 'playing');
+	});
+
+	it(' onPlayPause dispatches playing state', function() {
+		scope.$apply(function(){
+			scope.playState = 'playing';
+		});
+		spyOn(rootScope, '$broadcast');
+		scope.onPlayPause();
+		expect(scope.$broadcast).toHaveBeenCalledWith('playStateEvent', 'paused');
+	});
+
+  });
+});
