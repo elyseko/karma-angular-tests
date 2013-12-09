80b76d502e2706cb45b1c6df8853488d82256652
Elyse <elyse@realeyes.com>
2013-12-09 10:21:57 -0700

    updated karma.conf.js and removed irrelevant testing files

--- article1/config/karma.conf.js
+++ article1/config/karma.conf.js
@@ -1,6 +1,10 @@
 module.exports = function(config){
-    config.set({
+  config.set({
     basePath : '../',
+
+    preprocessors: {
+      '**/*tpl.html': ['ng-html2js']
+    },
 
     files : [
       'app/lib/angular/angular.js',
@@ -18,9 +22,27 @@ module.exports = function(config){
 
     autoWatch : true,
 
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_DEBUG,
+
     frameworks: ['jasmine'],
 
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
     browsers : ['Chrome'],
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
 
     plugins : [
             'karma-junit-reporter',
